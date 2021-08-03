8a308524c3d57c6994feab2bf2836945c00328b0
Sahithi Paladi <sahithi.p@tvsnext.io>
2021-08-03 18:12:35 +0530

    insering values into table

--- Addressbookservice.sql
+++ Addressbookservice.sql
@@ -13,3 +13,8 @@ zip BigInt,
 PhoneNumber BigInt,
 Email varchar(200)
 )
+------ UC3:Insert Values into Table ------
+Insert into AddressBookServiceTable(FirstName,LastName,Address,City,State,zip,PhoneNumber,Email) 
+values('Arjun','A','Nellore Road','Pamur','AndhraPradesh',523108,8858548393,'arjun@gmail.com'),
+('Vanitha','P','Gandhi Nagar','Ongole','AndhraPradesh',523001,9995559955,'vanitha@gamil.com'),
+('Sameera','S','Magunta Layout','Nellore','AndhraPradesh',524003,8393839475,'sameera@gmail.com');
