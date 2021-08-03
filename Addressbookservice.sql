ce491261aca8a1e1557fe9c91af2fb7d8809fe71
Sahithi Paladi <sahithi.p@tvsnext.io>
2021-08-03 18:09:19 +0530

    creating a table

--- Addressbookservice.sql
+++ Addressbookservice.sql
@@ -1,3 +1,15 @@
 
 ----------------UC1:Create Database----------------
 create database AddressBookService;
+----------------UC2:Create Table----------------
+create table AddressBookServiceTable
+(
+FirstName varchar(100),
+LastName varchar(100),
+Address varchar(250),
+City varchar(100),
+State varchar(100),
+zip BigInt,
+PhoneNumber BigInt,
+Email varchar(200)
+)
